9cd2860a3b05dae871601ca79294c686ba86f933
johanbujari <johanbujari@outlook.com>
2023-01-12 16:05:51 +0100

    person

--- ReactApps/puttingittogether/src/components/Person.js
+++ ReactApps/puttingittogether/src/components/Person.js
@@ -1,19 +1,55 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-const Person = (props) => {
-  const [age, setAge] = useState(props.age);
-  return (
-    <div>
-      <h1>
-        {props.firstName} {props.lastName}
-      </h1>
-      <p>Age: {age}</p>
-      <button onClick={(event) => setAge(age + 1)}>
-        Birthday button for {props.firstName} {props.lastName}
-      </button>
-      <p>Hair Color: {props.color}</p>
-    </div>
-  );
-};
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: props.age
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ age: this.state.age + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.props.firstName} {this.props.lastName}
+        </h1>
+        <p>Age: {this.state.age}</p>
+        <button onClick={this.handleClick}>
+          Birthday button for {this.props.firstName} {this.props.lastName}
+        </button>
+        <p>Hair Color: {this.props.color}</p>
+      </div>
+    );
+  }
+}
 
 export default Person;
+
+
+
+
+
+// import React, { useState } from "react";
+
+// const Person = (props) => {
+//   const [age, setAge] = useState(props.age);
+//   return (
+//     <div>
+//       <h1>
+//         {props.firstName} {props.lastName}
+//       </h1>
+//       <p>Age: {age}</p>
+//       <button onClick={(event) => setAge(age + 1)}>
+//         Birthday button for {props.firstName} {props.lastName}
+//       </button>
+//       <p>Hair Color: {props.color}</p>
+//     </div>
+//   );
+// };
+
+// export default Person;
